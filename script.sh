ab9900cd6d7c4f94e2b8e0ccd4fb017b197d1c9c
Gerald Ng <ngjingsiang@gmail.com>
2019-09-10 11:40:16 +0800

    Swallow status 409 for idempotent POST to same resource

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+status=0
 IFS=',' read -ra CHARTS <<< "$PLUGIN_CHARTS"
 for chart in "${CHARTS[@]}"; do
-    helm push --username $PLUGIN_USERNAME --password $PLUGIN_PASSWORD $chart $PLUGIN_REPO_URL
+    # swallow 409 conflict error for idempotent POST-ing to
+    # existing resource.
+    if ! helm push $chart $PLUGIN_REPO_URL 2>&1 | grep -iEq "Error: 409|Done"; then
+        status=1
+    fi
 done
+
+exit $status
